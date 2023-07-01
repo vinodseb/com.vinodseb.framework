81a8737009b0de05c16a474c534728696de654e8
vinodseb <vinod.sabastian@gmail.com>
2023-07-01 12:41:54 +0200

    Return rendered content on post request

--- framework.renderer/src/main/kotlin/com/vinodseb/framework/Routing.kt
+++ framework.renderer/src/main/kotlin/com/vinodseb/framework/Routing.kt
@@ -2,15 +2,13 @@ package com.vinodseb.framework
 
 import com.vinodseb.framework.model.Page
 import io.ktor.server.application.*
+import io.ktor.server.mustache.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Route.pageRoute() = get("/page") {
-    call.respond(Page("Some Random Title"))
-}
-
-//.post("/page") {
-//    val page = call.receive<Page>()
-//    // TODO add some data processing example here
-//    call.respond(MustacheContent("page.hbs", mapOf("page" to page)))
-//}
+fun Route.pageRoute() =
+        post("/page") {
+            val page = call.receive<Page>()
+            call.respond(MustacheContent("page.hbs", mapOf("page" to page)))
+        }
