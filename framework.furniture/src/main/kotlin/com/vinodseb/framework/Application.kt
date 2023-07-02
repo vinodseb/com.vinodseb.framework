1d0dcefae89defd07139af7f7e693ee521219940
vinodseb <vinod.sabastian@gmail.com>
2023-07-02 22:58:58 +0200

    Add router test for furniture

--- framework.furniture/src/main/kotlin/com/vinodseb/framework/Application.kt
+++ framework.furniture/src/main/kotlin/com/vinodseb/framework/Application.kt
@@ -15,5 +15,6 @@ fun Application.module() {
 
     routing {
         swaggerRoute()
+        testRoute()
     }
 }
--- framework.furniture/src/main/kotlin/com/vinodseb/framework/Router.kt
+++ framework.furniture/src/main/kotlin/com/vinodseb/framework/Router.kt
@@ -1,7 +1,15 @@
 package com.vinodseb.framework
 
+import io.ktor.http.*
+import io.ktor.server.application.*
 import io.ktor.server.plugins.swagger.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.swaggerRoute() =
     swaggerUI(path = "/swagger", swaggerFile = "openapi/documentation.yaml")
+
+fun Route.testRoute() =
+    get("/test") {
+        call.respondText("<html><body>success</body></html>", ContentType.Text.Html)
+    }
