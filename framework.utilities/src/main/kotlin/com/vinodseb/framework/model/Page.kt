2aec16e7ac5110a0d0ef39c89a3ba4879b0ad30f
vsabastian <sabastian.vinodbella@springernature.com>
2023-07-04 12:53:54 +0200

    Add banner template

--- framework.utilities/src/main/kotlin/com/vinodseb/framework/model/Page.kt
+++ framework.utilities/src/main/kotlin/com/vinodseb/framework/model/Page.kt
@@ -1,29 +1,14 @@
 package com.vinodseb.framework.model
 
-import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Page(
     val title: String,
-    val header: Component? = null,
-    val footer: Footer? = null,
-    val body: Body? = null
-)
-
-@Serializable
-data class Footer(
-    val corporate: Component,
-    val global: Component
-)
-
-@Serializable
-data class Body(
-    val top: Region?,
-    val left: Region?,
-    val main: Region?,
-    val right: Region?,
-    val bottom: Region?
+    val header: Component = mapOf(),
+    val banner: Component = mapOf(),
+    val main: Region = listOf(),
+    val sidebar: Region = listOf()
 )
 
 typealias Region = List<Component>
