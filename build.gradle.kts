dedcef711a3050480a1d99fbb55bd39f27341a96
vsabastian <sabastian.vinodbella@springernature.com>
2023-07-06 11:46:22 +0200

    Move detekt version to buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("plugin.serialization") version kotlin_version
     id("com.github.ben-manes.versions") version versions_version
     id("org.jlleitschuh.gradle.ktlint") version klint_version
-    id("io.gitlab.arturbosch.detekt") version "1.23.0"
+    id("io.gitlab.arturbosch.detekt") version detekt_version
 }
 
 allprojects {
