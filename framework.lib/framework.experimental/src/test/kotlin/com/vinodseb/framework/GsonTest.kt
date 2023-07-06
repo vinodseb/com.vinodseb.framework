0937ed63ccfd414e9577343900c1cc702ff17a1b
vinodseb <vinod.sabastian@gmail.com>
2023-07-07 01:33:59 +0200

    Fix experiment fun pageToJson test

--- framework.lib/framework.experimental/src/test/kotlin/com/vinodseb/framework/GsonTest.kt
+++ framework.lib/framework.experimental/src/test/kotlin/com/vinodseb/framework/GsonTest.kt
@@ -45,8 +45,8 @@ class GsonTest {
             )
         ).run {
             assertEquals(
-                "{\"title\":\"\",\"header\":{},\"banner\":{},\"footer\"" +
-                    ":{\"content\":\"Footer content\"},\"main\":[],\"sidebar\":[]}",
+                "{\"lang\":\"en\",\"title\":\"\",\"header\":{},\"banner\":{}," +
+                        "\"footer\":{\"content\":\"Footer content\"},\"main\":[],\"sidebar\":[]}",
                 this
             )
         }
