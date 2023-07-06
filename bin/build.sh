3b3ef322b5d51765f427aa62aaf5085d87c195bb
vsabastian <sabastian.vinodbella@springernature.com>
2023-07-06 16:03:04 +0200

    Simplify base project shell scripts

--- bin/build.sh
+++ bin/build.sh
@@ -4,9 +4,5 @@ BASEDIR=$(dirname "$0")
 
 (
   cd "$BASEDIR/.." || exit; \
-  ./gradlew framework.backend:build \
-  framework.frontend:build \
-  framework.furniture:build \
-  framework.renderer:build \
-  framework.utilities:build
+  ./gradlew clean build
 )
--- bin/run.sh
+++ bin/run.sh
@@ -4,9 +4,5 @@ BASEDIR=$(dirname "$0")
 
 (
   cd "$BASEDIR/.." || exit; \
-  ./gradlew framework.backend:run \
-  framework.frontend:run \
-  framework.furniture:run \
-  framework.renderer:run
+  ./gradlew clean build run
 )
-
