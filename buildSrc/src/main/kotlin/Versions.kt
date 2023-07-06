353b102aaf25c0bf4e3fbec946534510123a0d7b
vsabastian <sabastian.vinodbella@springernature.com>
2023-07-06 09:17:32 +0200

    Add dependency upgrade script, upgrade

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 
 const val ktor_version = "2.3.2"
 const val logback_version = "1.4.8"
-const val kotlin_version = "1.9.0-RC"
+const val kotlin_version = "1.9.0"
 const val versions_version = "0.47.0"
---
+++ upgrade.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+#./gradlew dependencyUpdates -Drevision=release --warning-mode all
+./gradlew dependencyUpdates -Drevision=release --warning-mode all
