e218104681823af5781b71967ffb8761aea38b13
vsabastian <sabastian.vinodbella@springernature.com>
2023-07-06 16:44:31 +0200

    Upgrade klint-gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version kotlin_version
     kotlin("plugin.serialization") version kotlin_version
     id("com.github.ben-manes.versions") version versions_version
-    id("org.jlleitschuh.gradle.ktlint") version klint_version
+    id("org.jlleitschuh.gradle.ktlint") version klint_gradle_version
     id("io.gitlab.arturbosch.detekt") version detekt_version
 }
 
