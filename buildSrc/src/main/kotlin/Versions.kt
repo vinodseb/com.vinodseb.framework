816f9e021ec2fa2e66ad1835fda522fbbe9bb472
vinodseb <vinod.sabastian@gmail.com>
2023-08-15 08:48:52 +0200

    Dependency Upgrades
    Signed-off-by: vinodseb <vinod.sabastian@gmail.com>

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,8 +1,8 @@
 
-const val ktor_version = "2.3.2"
-const val logback_version = "1.4.8"
+const val ktor_version = "2.3.3"
+const val logback_version = "1.4.11"
 const val kotlin_version = "1.9.0"
 const val versions_version = "0.47.0"
-const val klint_gradle_version = "11.5.0"
+const val klint_gradle_version = "11.5.1"
 const val detekt_version = "1.23.1"
 const val kover_version = "0.7.3"
