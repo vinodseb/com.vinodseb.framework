6cf1788ef18f5513acaee5eb95add99543397af9
vinodseb <vinod.sabastian@gmail.com>
2023-08-25 12:10:54 +0200

    Upgrade dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 
 const val ktor_version = "2.3.3"
 const val logback_version = "1.4.11"
-const val kotlin_version = "1.9.0"
+const val kotlin_version = "1.9.10"
 const val versions_version = "0.47.0"
 const val klint_gradle_version = "11.5.1"
 const val detekt_version = "1.23.1"
