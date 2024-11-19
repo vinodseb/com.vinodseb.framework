44fe732cfbdd454bb5357da398255f0a270a3580
vinodseb <vinod.sabastian@gmail.com>
2024-11-19 06:57:44 +0100

    Upgrade java to jdk 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ subprojects {
 
     java {
         toolchain {
-            languageVersion.set(JavaLanguageVersion.of(17))
+            languageVersion.set(JavaLanguageVersion.of(21))
         }
     }
 }
